4e20270954ff54d47dd0aca292cc76dda5b2482e
Артем Карасюк <karasyuk.2018@azure.nstu.ru>
2021-10-22 23:22:06 +0700

    init

---
+++ secret_crocodile/secret_crocodile/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace secret_crocodile
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
